e74c75c4fba7809732d3bc3f9b72becfd9bd1edb
Joe French <joe.p.french@gmail.com>
2016-08-30 02:15:30 +0000

    Styling and DB column correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829235019) do
+ActiveRecord::Schema.define(version: 20160830020923) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "price"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "price",       default: 0
   end
 
 end
