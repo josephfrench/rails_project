cbfe9797844bfb23aea818136a20fa1b0f67956b
Joe French <joe.p.french@gmail.com>
2016-08-30 00:27:47 +0000

    4.2 Added Price Field to Products and Improved Styling

---
+++ db/migrate/20160829233848_rename_colours_in_products_to_price.rb
@@ -0,0 +1,5 @@
+class RenameColoursInProductsToPrice < ActiveRecord::Migration[5.0]
+  def change
+  	rename_column :products, :colour, :price
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827171745) do
+ActiveRecord::Schema.define(version: 20160829235019) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160827171745) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "price"
   end
 
 end
