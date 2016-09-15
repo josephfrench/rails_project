9a2f0ceb68352f62b78a15df85c8298be162fa90
Josephfrench <joe.p.french@gmail.com>
2016-09-15 00:29:25 +0000

    Fixed landing page bugs, reloaded vagrant

--- app/controllers/static_controller_controller.rb
+++ app/controllers/static_controller_controller.rb
@@ -4,4 +4,11 @@ class StaticControllerController < ApplicationController
 
   def contact
   end
+
+  def landing_page
+  	@products = Product.limit(3)
+  	@featured_product = Product.first
+  end
+
+
 end
