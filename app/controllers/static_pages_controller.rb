6adb0b9e8ccbd6af48bebded11ffaeb8a703f417
Josephfrench <joe.p.french@gmail.com>
2016-10-05 20:15:16 +0000

    4.5 cleaned up views with partials and optimized carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def landing_page
+  	@number = Product.id-1
   	@products = Product.limit(3)
-  	@featured_product = Product.last
+  	#@featured_product = Product.last
   end
 end
