3c57aedd58322503360ba1fc1ff98831d9157a6d
Josephfrench <joe.p.french@gmail.com>
2016-10-06 19:20:54 +0000

    4.5 fixed method issue in controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def landing_page
-  	@number = Product.id-1
   	@products = Product.limit(3)
+ 	@numbers = Product.ids
   	#@featured_product = Product.last
   end
 end
