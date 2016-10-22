1289ba330a72ad10595bc471579cc6fa5aada142
Josephfrench <joe.p.french@gmail.com>
2016-10-22 01:14:59 +0000

    Mailer correction

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,8 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
   @message = message
-    mail(:from => email,
+    mail(:from => @email,
         :to => 'joe.p.french@gmail.com',
-        :subject => "A new contact form message from #{name}")
+        :subject => "A new contact form message from #{@name}")
   end
 end
