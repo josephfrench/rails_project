1b7ae7b5c914453e5d5dbb73d72f0f33f5cf8f66
Josephfrench <joe.p.french@gmail.com>
2016-10-23 20:22:39 +0000

    5.7 comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
 	has_many :orders
+  has_many :comments
+  def highest_rating_comment
+    comments.rating_desc.pluck(:body).first
+  end
+  def lowest_rating_comment
+    comments.rating_desc.pluck(:body).last
+  end
 end
+
