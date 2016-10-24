f759660144d8144160299385c8e2b9999f742778
Josephfrench <joe.p.french@gmail.com>
2016-10-24 04:08:43 +0000

    5.8 star rating on comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,16 @@ class Product < ApplicationRecord
   def highest_rating_comment
     comments.rating_desc.pluck(:body).first
   end
+  def highest_rating
+    comments.rating_desc.pluck(:rating).first
+  end     
   def lowest_rating_comment
     comments.rating_asc.pluck(:body).first
   end
+  def lowest_rating
+    comments.rating_asc.pluck(:rating).first
+  end   
+  def average_rating
+    comments.average(:rating).round(1)
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :products
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
@@ -19,6 +20,10 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
+
+  resources :products do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
