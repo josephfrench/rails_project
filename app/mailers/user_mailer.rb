593a533da5ff39df86c2a47bd068e3a6fd282167
Josephfrench <joe.p.french@gmail.com>
2016-10-28 02:34:09 +0000

    web app updates - fixed reg mailer and added can ability

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
         :to => 'joe.p.french@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "DiscoverLocal"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
+  end
 end
