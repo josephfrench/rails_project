62479af190fc26ee9f001637caf7f1a0ee625f53
Josephfrench <joe.p.french@gmail.com>
2016-11-05 19:58:00 +0000

    6.9 jquery plugins and JS

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,17 @@
 $(document).on('turbolinks:load', function(){
+  //Raty
   $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
   $('.rated').raty({ path: '/assets',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
     }
+  });
+  //Image Zoom
+  $('.img-zoom').elevateZoom({
+    scrollZoom : true,
+    zoomType : "lens",
+    lensShape : "round",
+    lensSize : 250
   });
 });
